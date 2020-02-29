e8ddf1e3682ca4e1b342f95bbfcb8807105f0095
alumno <alumno>
2020-02-29 10:42:59 -0300

    repasodelsabado

--- 9-Parciales/2019_1Cuatri_Recu_fuentes/cinco.js
+++ 9-Parciales/2019_1Cuatri_Recu_fuentes/cinco.js
@@ -1,4 +1,47 @@
 function mostrar()
 {
+var nombre;
+var cantidadPersonas;
+var dias;
+var respuesta;
+var contador=0;
+var formadepago;
+var acumulador;
+ while(respuesta=="si")
+ {
 
+	nombre=prompt("Ingrese su nombre");
+ 	while(isNaN(nombre))
+
+ 	cantidadPersonas=prompt("Ingrese cuantas personas se van a quedar");
+ 	cantidadPersonas=parseInt(cantidadPersonas);
+ 	while(isNaN(cantidadPersonas))
+ 		
+
+
+ 	dias=prompt("Ingrese cuantos dias se va a quedar");
+ 	dias=parseInt(dias);
+ 	while(!isNaN(dias))
+ 		if (contador=0)
+ 		{
+ 			contador++;
+ 			contador=contador+dias;
+ 		}
+ 		
+ }
+ 
+ respuesta=confirm("Desea seguir cargando datos");
+document.write(<Sc>);
 }
+
+ la gestión de un hotel,
+ingresar los siguientes datos validados de una reserva
+nombre del huésped
+cantidad de personas 
+cantidad de dia de estadia 
+forma de //pago (efectivo , tarjeta o QR)
+
+informar el huésped que trajo más personas en una sola reserva.
+la cantidad de personas que se quedaron más días
+la forma de pago más utilizada.
+el promedio de cantidad de días por reserva
--- 9-Parciales/2019_1Cuatri_Recu_fuentes/cuatro.js
+++ 9-Parciales/2019_1Cuatri_Recu_fuentes/cuatro.js
@@ -1,6 +1,6 @@
 function mostrar()
 {
-var nombre;
+	var nombre;
 	var edad;
 	var sexo;
 	var temperatura;
